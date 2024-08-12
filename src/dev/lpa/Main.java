b26a3d0d7908052ed7216e0cecd7a2cf27f51246
brian070069 <kkipngeno409@gmail.com>
2024-08-12 23:45:13 +0300

    second commit

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -7,24 +7,34 @@ record GroceryItem(String name,String type,int count){
     public GroceryItem(String name) {
         this(name,"DAIRY",1);
     }
+
+    public String toString(){
+        return String.format("%d %s in %s",count,name.toUpperCase(),type);
+    }
 }
 public class Main {
     public static void main(String[] args) {
-        GroceryItem[] groceryArray= new GroceryItem[3];
-       groceryArray[0] = new GroceryItem("milk");
-       groceryArray[1] = new GroceryItem("apples","PRODUCE",6);
-       groceryArray[2]= new GroceryItem("oranges","PRODUCE",5);
-       System.out.println(Arrays.toString(groceryArray));
 
-        ArrayList objectList=new ArrayList();
-        objectList.add(new GroceryItem("Butter"));
-        objectList.add("Yogurt");
+        GroceryItem[] groceryArray=new GroceryItem[3];
+        groceryArray[0]=new GroceryItem("milk");
+        groceryArray[1]=new GroceryItem("apples","PRODUCE",6);
+        groceryArray[2]=new GroceryItem("oranges","PRODUCE",5);
+
+        System.out.println(Arrays.toString(groceryArray));
+
+//        ArrayList objectList= new ArrayList();
+//        objectList.add(new GroceryItem("Butter"));
+//        objectList.add("Yogurt");
 
         ArrayList<GroceryItem> groceryList= new ArrayList<>();
         groceryList.add(new GroceryItem("Butter"));
         groceryList.add(new GroceryItem("milk"));
-        groceryList.add(new GroceryItem("oranges","PRODUCE",5));
+        groceryList.add(new GroceryItem("Oranges","PRODUCE",5));
+        groceryList.set(0,new GroceryItem("apples","PRODUCE",6));
         System.out.println(groceryList);
+        groceryList.remove(1);
+        System.out.println(groceryList);
+
 
     }
 
