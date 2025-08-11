5c9d4663ae17991893073257632db473135ea50b
brian070069 <kkipngeno409@gmail.com>
2025-08-11 13:04:24 +0300

    exploring different ways of retrieving element in an ArrayList, also explloring ways of removing elements in an array

--- src/dev/lpa/MoreLists.java
+++ src/dev/lpa/MoreLists.java
@@ -1,6 +1,7 @@
 package dev.lpa;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class MoreLists {
@@ -23,6 +24,39 @@ public class MoreLists {
         groceries.addAll(nextList);
         System.out.println(groceries);
 
+        System.out.println("Third element:"+groceries.get(2));
+
+        if(groceries.contains("ketchup")) {
+            System.out.println("List contains Mustard");
+        }
+
+        groceries.add("bananas");
+
+        System.out.println("First ="+ groceries.indexOf("bananas"));
+        System.out.println("Last ="+groceries.lastIndexOf("bananas"));
+        System.out.println(groceries);
+        groceries.remove(2);
+        System.out.println(groceries);
+        groceries.remove("mustard");
+        System.out.println(groceries);
+
+        groceries.removeAll(List.of("bananas","eggs","grapes"));
+        System.out.println(groceries);
+
+        groceries.addAll(List.of("cheese","millet","wheat","barley","rice"));
+        System.out.println(groceries);
+
+        groceries.retainAll(List.of("cheese","pickles","barley","wheat","ketchup"));
+        System.out.println(groceries);
+
+        groceries.clear();
+        System.out.println(groceries);
+        System.out.println("is Empty ="+groceries.isEmpty());
+
+
+        groceries.addAll(List.of("apples","milk","cheese","mustard"));
+        groceries.addAll(Arrays.asList("eggs","pickles","mustard","ham"));
+        System.out.println(groceries);
 
     }
 }
